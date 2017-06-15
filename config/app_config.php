8fc0c68492467da3c1fda5c1bd4a379b837a94a4
2Q11 <ji6676@naver.com>
2017-06-15 08:42:44 +0000

    20170615

--- config/app_config.php
+++ config/app_config.php
@@ -7,6 +7,21 @@ defined('_IP_HEADER_ARRAY_') OR define('_IP_HEADER_ARRAY_',  [
 																,'HTTP_X_FORWARDED', 'HTTP_FORWARDED_FOR'
 																,'HTTP_FORWARDED', 'REMOTE_ADDR']);
 defined('_HOME_') OR define('_HOME_', $_SERVER['DOCUMENT_ROOT'] );
+defined('_AUTH_KEY_') OR define('_AUTH_KEY_', '1345lashdf57sjv8234752jkfnxvhre9046idfhlhklsfd8');
+
+defined('_ERROR_MSG_') OR define('_ERROR_MSG_', [	
+													'KR' 	=> []
+													,'ENG'	=> [
+																 '403' => 'auth false'
+																,'404' => 'Not Found'
+																,'500' => 'internal server error'
+													]
+						  ]);
+
+$REQUEST_URI	= $_SERVER['REQUEST_URI'];
+$SP_URI			= explode('/', $REQUEST_URI);
+
+defined('_PATH_') OR define('_PATH_', $SP_URI[1]);
 
 require _HOME_.'/vendor/autoload.php';
 
